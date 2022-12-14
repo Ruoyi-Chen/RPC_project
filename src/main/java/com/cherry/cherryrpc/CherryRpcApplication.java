fc34e5364ece1764d2f83f878a4c0e5de5a9ba7f
cherry <chenruoyisz@gmail.com>
2022-12-14 14:42:00 +0800

    Initial commit

---
+++ src/main/java/com/cherry/cherryrpc/CherryRpcApplication.java
@@ -0,0 +1,13 @@
+package com.cherry.cherryrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherryRpcApplication.class, args);
+    }
+
+}
